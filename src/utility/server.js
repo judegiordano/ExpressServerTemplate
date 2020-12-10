22c2ddb5d1e977fa37ae8cf9b2079112fe8e38f7
JudeBoy <judegiordano@gmail.com>
2020-12-10 11:16:23 -0500

    Fix Disabled-By

--- src/utility/server.js
+++ src/utility/server.js
@@ -4,11 +4,15 @@ const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const app = require("express")();
 
-app.disable("x=powered=by");
+app.disable("x-powered-by");
 app.use(cors());
 app.use(helmet());
 app.set("json spaces", 2);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-module.exports = { app };
+module.exports = {
+	app
+};
