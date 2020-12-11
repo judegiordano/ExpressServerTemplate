4072fcdc25b7b3a45d7d45c07865c6aa8e9b5a28
JudeBoy <judegiordano@gmail.com>
2020-12-11 16:46:25 -0500

    Implement Server Clustering

--- src/utility/server.js
+++ src/utility/server.js
@@ -1,16 +1,24 @@
 require("dotenv").config();
 const cors = require("cors");
 const helmet = require("helmet");
+const morgan = require("morgan");
+const compression = require("compression");
 const bodyParser = require("body-parser");
 const app = require("express")();
 
 app.disable("x-powered-by");
-app.use(cors());
-app.use(helmet());
 app.set("json spaces", 2);
+app.use(morgan("common"));
+app.use(helmet());
 app.use(bodyParser.json());
+app.use(compression());
 app.use(bodyParser.urlencoded({
 	extended: true
+}));
+app.use(cors({
+	origin: ["http://localhost:3001"],
+	methods: ["GET", "POST"],
+	allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 module.exports = {
