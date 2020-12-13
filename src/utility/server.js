74159b8d1b0d3856db054031c6d4e9504146c848
JudeBoy <judegiordano@gmail.com>
2020-12-12 23:51:38 -0500

    Allow Wildcard Origin For Now

--- src/utility/server.js
+++ src/utility/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(cors({
-	origin: ["http://localhost:3001"],
+	origin: "*",
 	methods: ["GET", "POST"],
 	allowedHeaders: ["Content-Type", "Authorization"]
 }));
